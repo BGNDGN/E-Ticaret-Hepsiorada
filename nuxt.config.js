b044f7f7c1001c338275a1578369c166d2f19a0f
Burak <burakgundogan25@gmail.com>
2024-12-29 21:39:35 +0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ETicaret',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'category-product',
+        path: '/categories/:id/product/:product',
+        component: resolve(__dirname, 'pages/categories/product/_product.vue')
+      })
+    }
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  server: {
+    host: '0.0.0.0', // Tüm ağ arayüzlerinden erişim
+    port: 3000, // Varsayılan port
+  },
+}
---
+++ pages/sepet.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="main">
+      <a href="http://192.168.1.67:3000/" class="titlewebid2">Hepsiorada</a>
+      <h1>Sepetim</h1>
+      <button class="clear" @click="clearCart">Sepeti Temizle</button>
+      <hr>
+      <ul v-if="cart.length">
+        <li v-for="(product, index) in cart" :key="index">
+          <img :src="product.img" alt="Ürün Resmi" />
+          <h3>{{ product.title }}</h3>
+          <p>Fiyat: {{ product.price * product.quantity }} TL</p>
+  
+          <div id="featurecontainer">
+            <p class="feature">Özellikler</p>
+            <hr>
+            <ul>
+              <li v-for="(spec, i) in product.ozellik" :key="i">
+                {{ spec }}
+              </li>
+            </ul>
+            <div>
+              <button class="arttir"@click="changeQuantity(index, 'increase')">Arttır</button>
+              <button class="azalt" @click="changeQuantity(index, 'decrease')" :disabled="product.quantity <= 1">Azalt</button>
+              <button @click="removeFromCart(index)">Ürünü Kaldır</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <p v-else>Sepetinizde ürün bulunmamaktadır.</p>
+    </div>
+  </template>
+  
+  <script>
+  import "@/pages/styles/sepet.css"
+  
+  export default {
+    data() {
+      return {
+        cart: []
+      };
+    },
+    mounted() {
+      this.cart = JSON.parse(localStorage.getItem("cart")) || [];
+  
+      this.cart.forEach(product => {
+        if (!product.quantity) {
+          product.quantity = 1;  
+        }
+      });
+    },
+    methods: {
+      removeFromCart(index) {
+        this.cart.splice(index, 1); 
+        localStorage.setItem("cart", JSON.stringify(this.cart)); 
+        alert("Ürün sepetinizden kaldırıldı!");
+      },
+  
+      changeQuantity(index, action) {
+        const product = this.cart[index];
+  
+        if (action === 'increase') {
+          product.quantity++; 
+          let cart = [];
+          localStorage.setItem("cart", JSON.stringify(this.cart));
+  
+        } else if (action === 'decrease' && product.quantity > 1) {
+          product.quantity--; 
+        }
+  
+        localStorage.setItem("cart", JSON.stringify(this.cart));
+      },
+  
+      clearCart() {
+        let cart = [];
+        localStorage.clear();    
+        localStorage.setItem("cart", JSON.stringify(cart));
+        console.log("Sepet temizlendi!");
+        alert("Sepetiniz Temizlendi !")
+      }
+    }
+  }
+  </script>
